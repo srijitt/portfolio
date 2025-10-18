2c8d5302ab70f6f7ab6346be2f2aef570ce70f2d
srijitt <srijit.chakraborty154@gmail.com>
2025-10-18 10:13:08 +0530

    updated work exp

--- src/info/Info.js
+++ src/info/Info.js
@@ -5,25 +5,12 @@ import smart from "../img/switch.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
 export let colors = ["rgb(222, 156, 255)", "rgb(212, 126, 255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-/* 
-This variable will change the layout of the website from multipage to single, scrollable page
-*/
+
 export let singlePage = false;
 
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
+
 
 export const info = {
     firstName: "Srijit",
@@ -40,7 +27,7 @@ export const info = {
         },
         {
             emoji: "💼",
-            text: "research Intern at ibm"
+            text: "previously interned at IBM Research and IEMA r&d"
         },
         {
             emoji: '☕',
